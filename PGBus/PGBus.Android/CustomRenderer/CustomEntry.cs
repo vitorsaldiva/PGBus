1edd3ee8db6a1b8c0481601bd912c38c08c2c873
Vitor Augusto Saldiva Soares <vitorsaldiva@hotmail.com>
2019-08-10 01:38:36 -0300

    [Layout ]Inclusão custom entry; Ajustes no layout do card de infos dos veiculos; Inclusão pesquisa linhas

---
+++ PGBus/PGBus.Android/CustomRenderer/CustomEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using PGBus.Droid.CustomRenderer;
+using PGBus.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntryRenderer),typeof(CustomEntry))]
+namespace PGBus.Droid.CustomRenderer
+{
+    
+    public class CustomEntry : EntryRenderer
+    {
+        public CustomEntry(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackground(null);
+            
+        }
+    }
+}
